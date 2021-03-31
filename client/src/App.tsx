43d48d0aa1fc3544a12b85c09ce711b62d2cfe64
benjaminlee1 <ben.lbx@gmail.com>
2021-03-31 08:29:08 -0600

    Facilitate simple base communication between client and server in local env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import Request from "request";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  const [username, setUserName] = useState("");
+  async function getLastFiveUserMatches() {
+    const resp = await fetch("http://localhost:5001/getLastFiveMatches", {
+      method: "GET",
+      mode: "no-cors",
+    });
+
+    console.log("Hello\n");
+    console.log(resp);
+  }
+
+  return (
+    <div>
+      <TextField
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      >
+        {username}
+      </TextField>
+      <Button onClick={getLastFiveUserMatches}>Search</Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ server/endpoints/getLastFiveMatches.ts
@@ -0,0 +1,1 @@
+export async function getLastFiveMatches(username) {}
